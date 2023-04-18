c28c1e1fcbf88fbfc60ec72b27521730dc8bbade
Yuu0309 <79899789+Yuu0309@users.noreply.github.com>
2023-04-18 22:46:49 +0800

    Update Deck.h

--- Deck.h
+++ Deck.h
@@ -14,7 +14,7 @@ public:
     Card dealCard(); // 發一張牌
     int cardsLeft(); // 剩餘未發出的排
 private:
-    Card cards_[52]; // 牌堆中所有的牌
+    Card cards[52]; // 牌堆中所有的牌
     int currentCard; // 已經發出的牌的數量
 };
 
