60ec81ca9b7aea9901f9151b43f5152aee2aa361
Yhaocccc <126649276+Yhaocccc@users.noreply.github.com>
2023-04-18 22:49:33 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 int main() {
-    int pokerCin = 49; // 用於使用ASCII碼的變數
-    string pokerOnScreen[52]; // 撲克牌陣列
+    int pokerOnScreenCin = 49; // 用於使用ASCII碼的變數
+    string pokerOnScreen[52]; // 顯示於螢幕的撲克牌陣列
 
     for (int i = 0; i < 52; i++) {
         if (i % 13 == 0) {
@@ -21,11 +21,27 @@ int main() {
         }
         else if (i % 13 == 12) {
             pokerOnScreen[i] = "K"; //設第13個數字為K
-            pokerCin = 48;
+            pokerOnScreenCin = 48;
         }
         else {
-            pokerOnScreen[i] = (char)pokerCin; //其餘數字利用ASCII碼輸入
+            pokerOnScreen[i] = (char)pokerOnScreenCin; //其餘數字利用ASCII碼輸入
         }
-        pokerCin++;
+        pokerOnScreenCin++;
     }// 將A~K輸入進陣列當中 共四次
+
+    int poker[52]; //實際上玩家手上撲克牌的點數陣列
+
+    for (int i = 0; i < 52; i++) {
+        if (i % 13 == 9 || i % 13 == 10 || i % 13 == 11 || i % 13 == 12) {
+            poker[i] = 10; //10, J, Q, K為點數10
+        }
+        else {
+            poker[i] = (i + 1) % 13; //剩餘數字照常輸入
+        }
+    }
+/*
+    for (int i = 0; i < 52; i++) {
+        cout << poker[i] << " ";
+    }
+*/
 }
