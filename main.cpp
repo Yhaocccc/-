4c47adc5da7f34cc98298458a654108812c82b06
elvis980721 <126648577+elvis980721@users.noreply.github.com>
2023-05-31 21:41:26 +0800

    輸入小數點取得卡牌及花色

--- main.cpp
+++ main.cpp
@@ -3,24 +3,36 @@
 using namespace std;
 
 int main() {
+     
+   for (int i = 0; i < 52; i++) {
+        cout << "Enter the value for poker card " << i << ": ";
+        cin >> poker[i];
+    }
+    for (int i = 0; i < 52; i++) {
+        int rank = static_cast<int>(poker[i]); // 取得整數部分(數字)
+        int suit = static_cast<int>((poker[i] - rank) * 10); // 取得小數部分(花色)
+        cout << "Poker card " << i << " has value: " << rank << "." << suit << endl;
+    }
+    return 0;
+}
     int pokerOnScreenCin = 49; // 用於使用ASCII碼的變數
     string pokerOnScreen[52]; // 顯示於螢幕的撲克牌陣列
 
     for (int i = 0; i < 52; i++) {
         if (i % 13 == 0) {
-            pokerOnScreen[i] = "A"; //設第1個數字為A
+            pokerOnScreen[i] = "A"; //設定1個數字為A
         }
         else if (i % 13 == 9) {
-            pokerOnScreen[i] = "10"; //設第10個數字為10
+            pokerOnScreen[i] = "10"; //設定10個數字為10
         }
         else if (i % 13 == 10) {
-            pokerOnScreen[i] = "J"; //設第11個數字為J
+            pokerOnScreen[i] = "J"; //設定11個數字為J
         }
         else if (i % 13 == 11) {
-            pokerOnScreen[i] = "Q"; //設第12個數字為Q
+            pokerOnScreen[i] = "Q"; //設定12個數字為Q
         }
         else if (i % 13 == 12) {
-            pokerOnScreen[i] = "K"; //設第13個數字為K
+            pokerOnScreen[i] = "K"; //設定13個數字為K
             pokerOnScreenCin = 48;
         }
         else {
@@ -31,6 +43,7 @@ int main() {
 
     int poker[52]; //實際上玩家手上撲克牌的點數陣列
 
+
     for (int i = 0; i < 52; i++) {
         if (i % 13 == 9 || i % 13 == 10 || i % 13 == 11 || i % 13 == 12) {
             poker[i] = 10; //10, J, Q, K為點數10
@@ -38,6 +51,7 @@ int main() {
         else {
             poker[i] = (i + 1) % 13; //剩餘數字照常輸入
         }
+
     } //輸入點數於陣列中
 /*
     for (int i = 0; i < 52; i++) {
